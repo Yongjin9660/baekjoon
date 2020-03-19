446ef4abeab362314a9e0b41a30ec135bd508348
YongjinKim <dydwls0669@gmail.com>
2020-03-20 04:44:33 +0900

    2020/03/20

--- check_2981.java
+++ check_2981.java
@@ -1,44 +1,39 @@
 
-
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
 public class check_2981 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
+
 		int n = Integer.parseInt(br.readLine());
 
 		int[] arr = new int[n];
 		for (int i = 0; i < n; i++)
 			arr[i] = Integer.parseInt(br.readLine());
-		Arrays.parallelSort(arr);
-		int min = 2;
-		int cnt;
-		int temp;
-		
-		while (true) {
-			temp = arr[0] % min;
-			cnt = 0;
-			for (int i = 1; i < n; i++) {
-				if (temp == arr[i] % min)
-					cnt++;
-			}
-			if (cnt == n - 1)
-				bw.write(min + " ");
-			min++;
-			if(min == arr[n-1]) break;
-		}
-		
-		bw.close();
-	
 
+		Arrays.sort(arr);
+
+		int temp = arr[1] - arr[0];
+
+		for (int i = 2; i < n; i++)
+			temp = gcd(temp, arr[i] - arr[i - 1]);
+
+		for (int i = 2; i <= temp; i++) {
+			if (temp % i == 0)
+				System.out.print(i + " ");
+		}
+
+	}
+
+	private static int gcd(int a, int b) {
+		if (a % b == 0)
+			return b;
+
+		return gcd(b, a % b);
 	}
 
 }
