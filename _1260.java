19e0b5ab740431e661b93929c1ece0e590895514
kimyongjin <dydwls0669@gmail.com>
2020-04-23 15:31:21 +0900

    2020/04/23

---
+++ _1260.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class _1260 {
+
+	static int map[][];
+	static boolean[] visit;
+	static int n, m, v;
+	static StringBuilder sb1 = new StringBuilder();
+	static StringBuilder sb2 = new StringBuilder();
+
+	private static void dfs(int i) {
+		visit[i] = true;
+		sb1.append(i + " ");
+
+		for (int j = 1; j < n + 1; j++) {
+			if (map[i][j] == 1 && visit[j] == false)
+				dfs(j);
+		}
+	}
+
+	private static void bfs(int i) {
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(i);
+		visit[i] = true;
+
+		while (!q.isEmpty()) {
+			int temp = q.poll();
+			sb2.append(temp + " ");
+			for (int j = 1; j <= n; j++) {
+				if (map[temp][j] == 1 && visit[j] == false) {
+					q.offer(j);
+					visit[j] = true;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		v = Integer.parseInt(st.nextToken());
+
+		map = new int[n + 1][n + 1];
+		visit = new boolean[n + 1];
+
+		for (int i = 0; i < n + 1; i++)
+			Arrays.fill(map[i], 0);
+
+		Arrays.fill(visit, false);
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			map[a][b] = 1;
+			map[b][a] = 1;
+		}
+
+		dfs(v);
+
+		System.out.println(sb1);
+		Arrays.fill(visit, false);
+		bfs(v);
+		System.out.println(sb2);
+	}
+
+}
--- _2309.java
+++ _2309.java
@@ -14,6 +14,39 @@ public class _2309 {
 
 		Arrays.parallelSort(arr);
 
+		int temp;
+		boolean check = false;
+
+		for (int i = 8; i >= 0; i--) {
+
+			temp = sum - arr[i];
+
+			for (int j = 8; j >= 0; j--) {
+				if (i == j)
+					continue;
+
+				temp -= arr[j];
+
+				if (temp == 100) {
+					arr[i] = 0;
+					arr[j] = 0;
+					check = true;
+					break;
+				} else
+					temp += arr[j];
+
+			}
+			if (check)
+				break;
+
+		}
+
+		for (int i = 0; i < 9; i++) {
+			if (arr[i] != 0) {
+				System.out.println(arr[i]);
+			}
+		}
+
 	}
 
 }
